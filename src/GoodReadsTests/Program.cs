2b2aed1647fc42cbd7410f25c6177179134b6bdb
Chris Jeffreys <chrisbjeffreys@gmail.com>
2020-03-23 08:53:15 -0500

    add logic and test projects

---
+++ src/GoodReadsTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoodReadsTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
